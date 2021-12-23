4607608e80626c7f16df846d9101218321deece7
Matias Menendez <matias.95@hotmail.es>
2021-12-22 21:25:49 -0300

    desafio 1

---
+++ desafio1matiasmenendez.js
@@ -0,0 +1,10 @@
+let nombre = prompt('Ingresar nombre')
+
+let ocupacion = prompt('De que trabajas?')
+
+let empresa = prompt('En que empresa trabajas?')
+
+const espacio = ' '
+
+alert (nombre + espacio + ' trabaja de ' + espacio + ocupacion + espacio + 'en' + espacio + empresa);
+
